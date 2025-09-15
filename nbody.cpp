07acbfb5ad772895b86db20e8fdde64009f3ae8a
SirBabyWipes <165241913+SirBabyWipes@users.noreply.github.com>
2025-09-15 23:21:22 +0000

    Add nbody.cpp

---
+++ nbody.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+
+
+const double G = 6.674e-11;
+const double softing = 0.0000001;
+
+class Particle {
+public:
+    double mass;
+    // (x, y, z)
+    std::vector<double> position;
+    std::vector<double> velocity;
+    std::vector<double> force;
+
+    Particle(double m, std::vector<double> p, std::vector<double> v) {
+        mass = m;
+        position = p;
+        velocity = v;
+        force = {0, 0, 0};
+    }
+};
+
+// random initialization
+std::vector<Particle> iRandom(int n){
+    std::vector<Particle> particles;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> rMass(1e10, 1e15);
+    std::uniform_real_distribution<> rPosition(-1000, 1000);
+    std::uniform_real_distribution<> rVelocity(-1, 1);
+
+    for(int i = 0; i < n; i++){
+        double m = rMass(gen);
+        std::vector<double> p = {rPosition(gen), rPosition(gen), rPosition(gen)};
+        std::vector<double> v = {rVelocity(gen), rVelocity(gen), rVelocity(gen)};
+        particles.push_back(Particle(m, p, v));
+    }
+    return particles;
+}
+
+// predefined initialization
+std::vector<Particle> iPredefined(){
+    std::vector<Particle> particles;
+
+    Particle sun(100, {0, 0, 0}, {0, 0, 0});
+    particles.push_back(sun);
+
+    Particle earth(10, {100, 0, 0}, {100, 0, 0});
+    particles.push_back(earth);
+
+    Particle moon(1, {105, 0, 0}, {100, 0, 0});
+    particles.push_back(moon);
+    
+    return particles;
+}
+
+// file initialization
+std::vector<Particle> iFile(const std::string &filename){
+    std::vector<Particle> particles;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Cannot open file " << filename << std::endl;
+        return particles;
+    }
+
+    int n;  
+    file >> n;
+
+    for (int i = 0; i < n; i++) {
+        double m, x, y, z, vx, vy, vz, fx, fy, fz;
+        file >> m >> x >> y >> z >> vx >> vy >> vz >> fx >> fy >> fz;
+        if (!file) break;
+
+        Particle p(m, {x, y, z}, {vx, vy, vz});
+        p.force = {fx, fy, fz};
+
+        particles.push_back(p);
+    }
+
+    return particles;
+}
+
+void forces(std::vector<Particle> &particles){
+    // set force to 0
+    for( Particle &p: particles){
+        p.force = {0, 0, 0};
+    }
+    // loop over all particles per particle
+    for(int i = 0; i < particles.size(); i++){
+        for (int j = i + 1; j < particles.size(); j++){
+            // calculate distance between particles
+            double dx = particles[j].position[0] - particles[i].position[0];
+            double dy = particles[j].position[1] - particles[i].position[1];
+            double dz = particles[j].position[2] - particles[i].position[2];
+            double distance = sqrt((dx*dx)+(dy*dy)+(dz*dz) + softing*softing);
+
+            // calculate gravitational force
+            double force = G * ((particles[i].mass * particles[j].mass) / (distance * distance));
+
+            // apply force to particles
+            particles[i].force[0] += force * (dx / distance);
+            particles[i].force[1] += force * (dy / distance);
+            particles[i].force[2] += force * (dz / distance);
+
+            particles[j].force[0] -= force * (dx / distance);
+            particles[j].force[1] -= force * (dy / distance);
+            particles[j].force[2] -= force * (dz / distance);
+        }
+    }
+}
+
+void updateVelocity(std::vector<Particle> &particles, double dt){
+    for (Particle &p : particles) {
+        p.velocity[0] += (p.force[0] / p.mass) * dt;
+        p.velocity[1] += (p.force[1] / p.mass) * dt;
+        p.velocity[2] += (p.force[2] / p.mass) * dt;
+    }
+}
+
+void updatePosition(std::vector<Particle> &particles, double dt){
+    for (auto &p : particles) {
+        p.position[0] += p.velocity[0] * dt;
+        p.position[1] += p.velocity[1] * dt;
+        p.position[2] += p.velocity[2] * dt;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // time and steps
+    //double dt = .1;
+    //int steps = 200;
+
+    // random initialization (n, dt, steps)
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0]
+                  << " <numParticles> <dt> <steps>\n";
+        return 1;
+    }
+    std::string firstArg = argv[1];
+    double dt = std::stod(argv[2]);
+    int steps = std::stoi(argv[3]);
+
+    std::vector<Particle> particles;
+
+    int n = std::stoi(argv[1]);
+    particles = iRandom(n);
+
+    // predefined initialization
+    //std::vector<Particle> particles = iPredefined();
+
+    // file initialization
+    //std::vector<Particle> particles = iFile("solar.tsv");
+
+    /* check step 1
+    std::cout << "Loaded " << particles.size() << " particles:\n";
+    for (size_t i = 0; i < particles.size(); i++) {
+        const Particle &p = particles[i];
+        std::cout << "Particle " << i << ":\n";
+        std::cout << "  Mass = " << p.mass << "\n";
+        std::cout << "  Position = (" 
+                  << p.position[0] << ", " 
+                  << p.position[1] << ", " 
+                  << p.position[2] << ")\n";
+        std::cout << "  Velocity = (" 
+                  << p.velocity[0] << ", " 
+                  << p.velocity[1] << ", " 
+                  << p.velocity[2] << ")\n";
+        std::cout << "  Force = (" 
+                  << p.force[0] << ", " 
+                  << p.force[1] << ", " 
+                  << p.force[2] << ")\n";
+    }
+    */
+
+    /* check step 2
+    std::cout << "Forces after calculation:\n";
+    for (size_t i = 0; i < particles.size(); i++) {
+        const Particle &p = particles[i];
+        std::cout << "Particle " << i 
+                  << " Force = (" 
+                  << p.force[0] << ", " 
+                  << p.force[1] << ", " 
+                  << p.force[2] << ")\n";
+    }
+    */
+
+    // console out
+    for (int step = 0; step < steps; step++) {
+        forces(particles);
+        updateVelocity(particles, dt);
+        updatePosition(particles, dt);
+
+        std::cout << "Step " << step << ":\n";
+        for (size_t i = 0; i < particles.size(); i++) {
+            const Particle &p = particles[i];
+            std::cout << "Particle " << i
+                      << " Position = (" << p.position[0] << ", "
+                                         << p.position[1] << ", "
+                                         << p.position[2] << ") "
+                      << " Velocity = (" << p.velocity[0] << ", "
+                                         << p.velocity[1] << ", "
+                                         << p.velocity[2] << ")\n";
+        }
+    }
+    
+    // file out
+    std::ofstream out("sim_output.tsv");
+    for (int step = 0; step < steps; step++) {
+        forces(particles);
+        updateVelocity(particles, dt);
+        updatePosition(particles, dt);
+
+        // number of particles
+        out << particles.size();
+
+        // particle info
+        for (const Particle &p : particles) {
+            out << "\t" << p.mass
+                << "\t" << p.position[0]
+                << "\t" << p.position[1]
+                << "\t" << p.position[2]
+                << "\t" << p.velocity[0]
+                << "\t" << p.velocity[1]
+                << "\t" << p.velocity[2]
+                << "\t" << p.force[0]
+                << "\t" << p.force[1]
+                << "\t" << p.force[2];
+        }
+
+        out << "\n";
+    }
+    out.close();
+
+    return 0;
+}
